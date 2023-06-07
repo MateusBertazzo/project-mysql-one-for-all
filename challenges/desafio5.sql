71d2132eff53889236463ce019d9649c939c1e4f
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-06-07 08:08:18 -0300

    resolve req 5

---
+++ challenges/desafio5.sql
@@ -0,0 +1,5 @@
+SELECT song.name_song AS cancao, COUNT(his.song_id) AS reproducoes
+FROM SpotifyClone.songs AS song
+INNER JOIN SpotifyClone.history AS his ON song.song_id = his.song_id
+GROUP BY cancao
+ORDER BY reproducoes DESC, cancao ASC LIMIT 2;
